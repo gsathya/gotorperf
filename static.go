0b6c0076d9f7d3c916bc71fe131360585198534c
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2015-05-26 14:10:53 +0530

    Measure perctime by implementing ReadFrom

--- static.go
+++ static.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/url"
 	"time"
@@ -12,11 +11,14 @@ import (
 )
 
 const (
-	uri     = "https://torperf.torproject.org:80/.50kbfile"
-	torAddr = "127.0.0.1:9050"
-	request = "GET %s HTTP/1.0\r\nPragma: no-cache\r\n" +
+	http_read_len = 16
+	uri           = "https://torperf.torproject.org:80/.50kbfile"
+	torAddr       = "127.0.0.1:9050"
+	request       = "GET %s HTTP/1.0\r\nPragma: no-cache\r\n" +
 		"Host: %s\r\n\r\n"
 )
+
+var startTime time.Time
 
 type StaticFileExperiment struct{}
 
@@ -29,12 +31,45 @@ func (s StaticFileExperiment) Run() (err error) {
 }
 
 type StaticFileDownload struct {
+	receivedBytes int
+	sentBytes     int
+
+	payloadSize  int
+	expectedSize int
+
 	datarequest  time.Duration
 	dataresponse time.Duration
 	datacomplete time.Duration
+	dataperctime []time.Duration
 }
 
-func (s StaticFileDownload) run() (err error) {
+func (s *StaticFileDownload) ReadFrom(r io.Reader) (err error) {
+	p := make([]byte, http_read_len)
+	s.dataperctime = make([]time.Duration, 9)
+	s.receivedBytes = 0
+	s.expectedSize = 51200
+	decile := -1
+
+	log.Println("reading response")
+	for {
+		n, err := r.Read(p)
+		s.receivedBytes += n
+		for s.receivedBytes < s.expectedSize &&
+			s.receivedBytes*10/s.expectedSize > decile+1 {
+			decile += 1
+			s.dataperctime[decile] = time.Since(startTime)
+		}
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			break
+		}
+	}
+	return nil
+}
+
+func (s *StaticFileDownload) run() (err error) {
 	t := torctl.NewTor(*conf.torPath)
 	if err = t.Start(); err != nil {
 		return err
@@ -50,6 +85,7 @@ func (s StaticFileDownload) run() (err error) {
 		return err
 	}
 
+	startTime = time.Now()
 	log.Println("creating socksfied dialer")
 	dialer, err := NewSocksfiedDialer(torAddr)
 	if err != nil {
@@ -67,13 +103,15 @@ func (s StaticFileDownload) run() (err error) {
 
 	log.Println("sending request")
 	fmt.Fprintf(conn, req)
+	s.datarequest = time.Since(startTime)
 
-	log.Println("reading response")
-	n, err := io.Copy(ioutil.Discard, conn)
+	err = s.ReadFrom(conn)
 	if err != nil {
 		return err
 	}
-	log.Println("total size of response", n)
+	s.datacomplete = time.Since(startTime)
+	log.Println("total size of response", s.receivedBytes)
+	log.Println("dataperctime", s.dataperctime)
 	return
 }
 
